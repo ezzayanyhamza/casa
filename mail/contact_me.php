aa331a0142f286ab8fd4f78ce507b3c4967dc636
ezzayanyhamza <58748289+ezzayanyhamza@users.noreply.github.com>
2020-12-26 14:08:42 +0100

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,16 +1,38 @@
 <?php
-<?php
+ include_once('../vendor/phpmailer/phpmailer/src/PHPMailer.php');
+ include_once('../vendor/phpmailer/phpmailer/src/SMTP.php');
+ include_once('../vendor/phpmailer/phpmailer/src/Exception.php');
 
-if (isset($_POST['submit'])){
-   $name=$_POST['name'];
-   $mailfrom=$_POST['email'];
-   $message=$_POST['message'];
+ $mail = new PHPMailer\PHPMailer\PHPMailer();
+ $mail->isSMTP();
+ $mail->SMTPAuth = true;
+ $mail->SetFrom('noreply@yourdomain.com','User');
 
-   $mailto ="hamzaziyani590@gmail.com";
-   $headers = "From:".$mailfrom;
-   $txt = "you have received mail from ".$name.".\.\".$message;
+// Check for empty fields
+if(empty($_POST['name']) ||
+ empty($_POST['email']) ||
+ empty($_POST['message']) ||
+ !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+ {
+ echo "No arguments Provided!";
+ return false;
+ }
 
-   mail($mailto, $txt ,$headers);
-   header("Location: index.html?mailsen");
-}			
+// Create the email and send the message
+ $host = "mail.{domain}.com";
+ $port = "587";/
+ $username = "noreply@yourdomain.com";
+ $password = "noreply";
+
+ $to = 'hamzaziyani590@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+ $name = strip_tags(htmlspecialchars($_POST['name']));
+ $email_address = strip_tags(htmlspecialchars($_POST['email']));
+ $phone = strip_tags(htmlspecialchars($_POST['phone']));
+ $message = strip_tags(htmlspecialchars($_POST['message']));
+ $email_subject = "Website Contact Form from: $name";
+ $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+ $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+ mail($to,$email_subject,$email_body,$headers,'-fnoreply@{domain}.com');
+ return true;
+
 ?>
